6df306d2d2c65a7594cbf1caa52e16a5bea1cda3
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-08-08 17:10:55 +0530

    Rendering registration page

--- L2_milestone/my_node_app1/index.js
+++ L2_milestone/my_node_app1/index.js
@@ -3,6 +3,9 @@ const http = require("http");
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
+let scriptContent = "";
+let cssContent = "";
 
 fs.readFile("home.html", function (err, home) {
   if (err) {
@@ -18,6 +21,22 @@ fs.readFile("project.html", function (err, project) {
   projectContent = project;
 });
 
+fs.readFile("form.js", function (err, form) {
+  if (err) throw err;
+  scriptContent = form;
+});
+
+fs.readFile("main.css", function (err, main) {
+  if (err) throw err;
+  cssContent = main;
+});
+
+fs.readFile("registration.html", function (err, registration) {
+  if (err) throw err;
+  registrationContent = registration;
+});
+
+
 http
   .createServer(function (request, response) {
     let url = request.url;
@@ -27,10 +46,23 @@ http
         response.write(projectContent);
         response.end();
         break;
+      case "/form.js":
+        response.write(scriptContent);
+        response.end();
+        break;
+      case "/main.css":
+        response.writeHeader(200, { "Content-Type": "text/css" });
+        response.write(cssContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
       default:
         response.write(homeContent);
         response.end();
         break;
     }
   })
-  .listen(3000);
+  .listen(3001);
