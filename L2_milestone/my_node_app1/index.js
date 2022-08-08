306e17bd9565439eceb965f1f5b5e8729188d706
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-08-08 15:23:16 +0530

    added first route

--- L2_milestone/my_node_app1/index.js
+++ L2_milestone/my_node_app1/index.js
@@ -1,13 +1,36 @@
 const fs = require("fs");
 const http = require("http");
 
+let homeContent = "";
+let projectContent = "";
+
 fs.readFile("home.html", function (err, home) {
-  if (err) throw err;
-  http
-    .createServer((request, response) => {
-      response.writeHeader(200, { "content-type": "text/html" });
-      response.write(home);
-      response.end();
-    })
-    .listen(3000);
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
 });
+
+fs.readFile("project.html", function (err, project) {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+http
+  .createServer(function (request, response) {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
